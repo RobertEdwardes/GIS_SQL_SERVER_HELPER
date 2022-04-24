d89ea13bc519cf790eaf01e0911305ba4fcf31bb
Robert Edwardes <rdedwardes@gmail.com>
2022-04-24 14:00:11 -0400

    add cli Node Edge JSON

--- cli.py
+++ cli.py
@@ -6,7 +6,7 @@ import geopandas as gpd
 import json
 
 @click.command()
-@click.option('--process', '-t', prompt='Choice Process', type=click.Choice(['Flat,']))
+@click.option('--process', '-t', prompt='Choice Process', type=click.Choice(['Create Table','Insert Data','Azure Connection','Index Table']))
 @click.option('--path',prompt='Path to working Directory', default=os.getcwd(), type=click.Path(exists=True))
 @click.option('--credtials',prompt='Path to SQL Credtials JSON', default=None, type=click.Path(exists=True))
 
@@ -14,7 +14,13 @@ def main(process, path, credtials)
 if not credtials:
     username = click.prompt()
     password = click.prompt()
-    databse = click.prompt()
+    database = click.prompt()
     table = click.prompt()
 else:
     df = pd.read_json(credtials)
+    result = df.to_json(orient="records")
+    parsed = json.loads(result)
+    username = parsed['username']
+    password = parsed['password']
+    database = parsed['database']
+    table = parsed['table']
